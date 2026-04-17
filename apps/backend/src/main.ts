45d3330baadfbee4c5ee3ca130538162a12f087a
korirke <ke.korir15@gmail.com>
2026-04-17 10:23:36 +0300

    feat: allowed cross origins

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 
   // CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'],
     credentials: true,
   });
 
