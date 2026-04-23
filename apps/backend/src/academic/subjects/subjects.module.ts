082e2fdd457c0fdcb9bfdd7d407c43d64ac94adb
korirke <ke.korir15@gmail.com>
2026-04-23 14:59:13 +0300

    feat(subjects): module wiring

---
+++ apps/backend/src/academic/subjects/subjects.module.ts
@@ -0,0 +1,15 @@
+/**
+ * @module SubjectsModule
+ */
+import { Module } from '@nestjs/common';
+import { SubjectsService } from './subjects.service';
+import { SubjectsController } from './subjects.controller';
+import { DatabaseModule } from '../../database/database.module';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [SubjectsController],
+  providers: [SubjectsService],
+  exports: [SubjectsService],
+})
+export class SubjectsModule {}
