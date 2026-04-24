e4c81094825cd90652edbaf7dafe4fb43518a0bf
korirke <ke.korir15@gmail.com>
2026-04-24 14:14:25 +0300

    feat(academic): wire years, terms, periods, classes, subjects, history, promotions, students

--- apps/backend/src/academic/academic.module.ts
+++ apps/backend/src/academic/academic.module.ts
@@ -1,15 +1,44 @@
+/**
+ * @module AcademicModule
+ * @description Aggregate module wiring every sub-module of the academic domain.
+ */
+
 import { Module } from '@nestjs/common';
 import { AcademicYearsModule } from './academic-years/academic-years.module';
+import { AcademicTermsModule } from './academic-terms/academic-terms.module';
+import { AcademicPeriodsModule } from './academic-periods/academic-periods.module';
 import { ClassesModule } from './classes/classes.module';
+import { SubjectsModule } from './subjects/subjects.module';
+import { StudentClassHistoryModule } from './student-class-history/student-class-history.module';
+import { PromotionsModule } from './promotions/promotions.module';
+import { StudentsModule } from './students/students.module';
+import { ProgressModule } from '../common/progress/progress.module';
+import { AcademicDashboardModule } from './academic-dashboard/academic-dashboard.module';
 
 @Module({
   imports: [
     AcademicYearsModule,
+    AcademicTermsModule,
+    AcademicPeriodsModule,
     ClassesModule,
+    SubjectsModule,
+    StudentClassHistoryModule,
+    PromotionsModule,
+    StudentsModule,
+    ProgressModule,
+    AcademicDashboardModule,
   ],
   exports: [
     AcademicYearsModule,
+    AcademicTermsModule,
+    AcademicPeriodsModule,
     ClassesModule,
+    SubjectsModule,
+    StudentClassHistoryModule,
+    PromotionsModule,
+    StudentsModule,
+    ProgressModule,
+    AcademicDashboardModule,
   ],
 })
 export class AcademicModule {}
