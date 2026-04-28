3adbe59abaf231737593e2f94bcc932add663186
korirke <ke.korir15@gmail.com>
2026-04-28 14:04:45 +0300

    feat(classes): module wiring

--- apps/backend/src/academic/classes/classes.module.ts
+++ apps/backend/src/academic/classes/classes.module.ts
@@ -1,8 +1,11 @@
+/**
+ * @file classes.module.ts
+ */
 import { Module } from '@nestjs/common';
-import { ClassesService } from './classes.service';
-import { ClassesController } from './classes.controller';
 import { DatabaseModule } from '../../database/database.module';
 import { ActivityModule } from '../../common/activity/activity.module';
+import { ClassesService } from './classes.service';
+import { ClassesController } from './classes.controller';
 
 @Module({
   imports: [DatabaseModule, ActivityModule],
